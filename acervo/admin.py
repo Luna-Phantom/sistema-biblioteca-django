adc3ea091e438107f22c86f1307691096db385a0
Luna <rlunamgt@gmail.com>
2026-09-20 14:35:57 -0300

    Adicionado models Membro e Exemplar

--- acervo/admin.py
+++ acervo/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Livro
+from .models import Exemplar, Livro, Membro
 
 admin.site.register(Livro)
+admin.site.register(Membro)
+admin.site.register(Exemplar)
--- acervo/models.py
+++ acervo/models.py
@@ -9,3 +9,23 @@ class Livro(models.Model):
 
   def __str__(self):
     return self.titulo
+
+
+class Membro(models.Model):
+  nome = models.CharField(max_length=150)
+  email = models.EmailField(unique=True)
+  telefone = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.nome
+
+
+class Exemplar(models.Model):
+  livro = models.ForeignKey(
+      Livro, on_delete=models.CASCADE, related_name='exemplares'
+  )
+  codigo_tombamento = models.CharField(max_length=50, unique=True)
+  disponivel = models.BooleanField(default=True)
+
+  def __str__(self):
+    return f'{self.livro.titulo} - Exemplar: {self.codigo_tombamento}'
