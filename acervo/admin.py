3006ccb093926021cf9b031ce09aab1d0c60aafd
Luna <rlunamgt@gmail.com>
2026-09-22 09:37:28 -0300

    Models de Emprestimo e Reserva

--- acervo/admin.py
+++ acervo/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Exemplar, Livro, Membro
+from .models import Emprestimo, Exemplar, Livro, Membro, Reserva
 
 admin.site.register(Livro)
 admin.site.register(Membro)
 admin.site.register(Exemplar)
+admin.site.register(Emprestimo)
+admin.site.register(Reserva)
--- acervo/models.py
+++ acervo/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 
 class Livro(models.Model):
   titulo = models.CharField(max_length=200)
@@ -29,3 +29,24 @@ class Exemplar(models.Model):
 
   def __str__(self):
     return f'{self.livro.titulo} - Exemplar: {self.codigo_tombamento}'
+
+class Emprestimo(models.Model):
+  exemplar = models.ForeignKey(Exemplar, on_delete=models.CASCADE)
+  membro = models.ForeignKey(Membro, on_delete=models.CASCADE)
+  data_emprestimo = models.DateField(default=timezone.localdate)
+  data_devolucao_prevista = models.DateField()
+  data_devolucao_real = models.DateField(null=True, blank=True)
+  multa = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
+
+  def __str__(self):
+    return f'Empréstimo: {self.exemplar.livro.titulo} para {self.membro.nome}'
+
+
+class Reserva(models.Model):
+  livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
+  membro = models.ForeignKey(Membro, on_delete=models.CASCADE)
+  data_reserva = models.DateTimeField(auto_now_add=True)
+  ativa = models.BooleanField(default=True)
+
+  def __str__(self):
+    return f'Reserva: {self.livro.titulo} por {self.membro.nome}'
